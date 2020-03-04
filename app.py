59e5fb86c6d7abeffce105083eec58c325a240b0
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2020-03-04 16:35:17 +0100

    getTime FONCTIONNE

--- app.py
+++ app.py
@@ -7,16 +7,29 @@ C = (4, 10)
 V1 = 2
 V2 = 1
 Amax = 1
-t0, t1, t2, t3, t4, t5 = 0
-AB, BC = 0
+AB = BC = 0
 
 def getTime():
     t0 = 0
-    t1 = V1 / Amax
-    t2 = ((Amax * (t1**2 / 2) - Amax * ((V2 - V1) / Amax)**2 / 2 - AB) / -V1) - t1
-    t3 = (V2 - V1) / Amax + t2
-    t4 = (-Amax * (-V2 / Amax)**2 / 2 - BC) / -V2 + t3
-    t5 = (-V2 / Amax) + t4
+    
+    delta_t0_t1 = V1 / Amax
+    delta_t2_t3 = (V2 - V1) / -Amax
+    
+    integ_t2_t3 = delta_t2_t3 + 1/2 * delta_t2_t3 * (V1 - V2)
+    integ_t0_t1 = (delta_t0_t1 * V1) / 2
+
+    delta_t1_t2 = (AB - integ_t2_t3 - integ_t0_t1) / V1
+
+    t1 = delta_t0_t1
+    t2 = t1 + delta_t1_t2
+    t3 = t2 + delta_t2_t3
+    
+    delta_t4_t5 = V2 / Amax
+    integ_t4_t5 = (delta_t4_t5 * V2) / 2
+    delta_t3_t4 = (BC - integ_t4_t5) / V2
+
+    t4 = t3 + delta_t3_t4
+    t5 = t4 + delta_t4_t5
 
     return (t0, t1, t2, t3, t4, t5)
 
@@ -72,7 +85,8 @@ if __name__ == "__main__":
     print("START")
     
     AB = math.sqrt((A[0] - B[0])**2 + (A[1] - B[1])**2)
-    BC = math.sqrt((B[0] - B[0])**2 + (C[1] - C[1])**2)
+    BC = math.sqrt((B[0] - C[0])**2 + (B[1] - C[1])**2)
     (t0, t1, t2, t3, t4, t5) = getTime()
 
+    print(AB, BC)
     print(t0, t1, t2, t3, t4, t5)
