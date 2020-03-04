b1facc651900fe00efd940313a27408886be1266
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2020-03-04 14:48:00 +0100

    getTime fini

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import afficheCourbesTP
+import math
 
-A = (2, 1)
-B = (3, 5)
-C = (1, 4)
+A = (20, 15)
+B = (30, 50)
+C = (8, 12)
 V1 = 10
 V2 = 10
 Amax = 10
 Vmax = 10
 t0, t1, t2, t3, t4, t5 = 0
+AB, BC = 0
 
 def getTime():
     t0 = 0
     t1 = V1 / Amax
     t2 = ((Amax * (t1**2 / 2) - Amax * ((V2 - V1) / Amax)**2 / 2 - AB) / -V1) - t1
     t3 = (V2 - V1) / Amax + t2
+    t4 = (-Amax * ((-V2 / Amax))**2 / 2 - BC) / -V2 + t3
+    t5 = (-V2/Amax) + t4
 
 #############################################################################
 # MAIN
@@ -21,4 +25,6 @@ def getTime():
 if __name__ == "__main__":
     print("START")
     
-
+    AB = math.sqrt((A(0) - B(0))**2 + (A(1) - B(1))**2)
+    BC = math.sqrt((A(0) - B(0))**2 + (A(1) - B(1))**2)
+    getTime()
