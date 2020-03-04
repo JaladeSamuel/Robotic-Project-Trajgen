8f65b6121ac9e1ab8e2118c434f1da7fd5ce763c
Samuel jalade <sam34440@hotmail.fr>
2020-03-04 15:06:42 +0100

    loi_de_mouvement_ds v2

--- app.py
+++ app.py
@@ -22,9 +22,9 @@ def loi_de_mouvement_ddS(t):
     a = 0 
     if t < t1:
         a = Amax
-    elif t>t2 and t<t3:
+    elif t>=t2 and t<t3:
         a = -Amax
-    elif t>t4 and t<t5:
+    elif t>=t4 and t<t5:
         a = -Amax
     return a
 
@@ -32,30 +32,33 @@ def loi_de_mouvement_dS(t):
     v = 0 
     if t < t1:
         v = Amax * t
-    elif t>t1 and t<t2:
+    elif t>=t1 and t<t2:
         v = V1
-    elif t>t2 and t<t3:
+    elif t>=t2 and t<t3:
         v = -Amax*t
-    elif t>t3 and t<t4:
+    elif t>=t3 and t<t4:
         v = V2
-    elif t>t4 and t<t5:
+    elif t>=t4 and t<t5:
         v = -Amax*t
     return v
 
 #loi_de_mouvement_S
-#Retourne la position x
+#INPUT:
+#    t: temps
+#OUTPUT:
+#    x: position
 def loi_de_mouvement_S(t):
     x = -1
     if t >= t0 and t < t1:
-        x =  1/2 * (Amax * t) * t #Amax * t = sd(t)
+        x =  1/2 * loi_de_mouvement_dS(t) * t #Amax * t = ds(t)
     elif t >= t1 and t < t2:
         x = V1 * t
     elif t >= t2 and t < t3:
-        x = -1/2 * (-Amax * t) * t #-Amax * t = sd(t)
+        x = -1/2 * loi_de_mouvement_dS(t) * t #-Amax * t = ds(t)
     elif t >= t3 and t < t4:
         x = V2 * t
     elif t >= t4 and t < t5:
-        x = -1/2 * -Amax * t #-Amax * t = sd(t)
+        x = -1/2 *loi_de_mouvement_dS(t) #-Amax * t = ds(t)
     return x
      
         
