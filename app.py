948b0668edb35a50a50800f46731c4eaa325d737
Aurelien Montmejat <aurelien.montmejat@gmail.com>
2020-03-04 15:08:29 +0100

    fixed un peu plus getTime

--- app.py
+++ app.py
@@ -1,23 +1,24 @@
 import afficheCourbesTP
 import math
 
-A = (20, 15)
-B = (30, 50)
-C = (8, 12)
-V1 = 10
-V2 = 10
-Amax = 10
+A = (0, 0)
+B = (0, 10)
+C = (4, 10)
+V1 = 2
+V2 = 1
+Amax = 1
 Vmax = 10
-t0, t1, t2, t3, t4, t5 = 0
-AB, BC = 0
+AB = BC = 0
 
 def getTime():
     t0 = 0
     t1 = V1 / Amax
     t2 = ((Amax * (t1**2 / 2) - Amax * ((V2 - V1) / Amax)**2 / 2 - AB) / -V1) - t1
     t3 = (V2 - V1) / Amax + t2
-    t4 = (-Amax * ((-V2 / Amax))**2 / 2 - BC) / -V2 + t3
-    t5 = (-V2/Amax) + t4
+    t4 = (-Amax * (-V2 / Amax)**2 / 2 - BC) / -V2 + t3
+    t5 = (-V2 / Amax) + t4
+
+    return (t0, t1, t2, t3, t4, t5)
 
 def loi_de_mouvement_ddS(t):
     a = 0 
@@ -35,6 +36,8 @@ def loi_de_mouvement_ddS(t):
 if __name__ == "__main__":
     print("START")
     
-    AB = math.sqrt((A(0) - B(0))**2 + (A(1) - B(1))**2)
-    BC = math.sqrt((A(0) - B(0))**2 + (A(1) - B(1))**2)
-    getTime()
+    AB = math.sqrt((A[0] - B[0])**2 + (A[1] - B[1])**2)
+    BC = math.sqrt((B[0] - B[0])**2 + (C[1] - C[1])**2)
+    (t0, t1, t2, t3, t4, t5) = getTime()
+
+    print(t0, t1, t2, t3, t4, t5)
